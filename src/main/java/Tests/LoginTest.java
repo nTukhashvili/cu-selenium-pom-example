f9b1fe69a38b18bce29b3df76778830003f996c4
nTukhashvili <ntukhashvili@cu.edu.ge>
2023-12-01 17:36:18 +0400

    added examples for allure and logback

--- src/main/java/Tests/LoginTest.java
+++ src/main/java/Tests/LoginTest.java
@@ -4,11 +4,17 @@ import Commons.BaseTest;
 import Pages.LoginPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class LoginTest extends BaseTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginTest.class);
+
     @Test
     public  void loginValid() {
+        logger.info("Running Test 1");
         driver.get("https://www.saucedemo.com");
         LoginPage loginP = new LoginPage(driver);
         loginP.enterUsername("standard_user");
@@ -19,6 +25,7 @@ public class LoginTest extends BaseTest {
 
     @Test
     public  void loginInvalid() {
+        logger.warn("WARNING WARNING");
         driver.get("https://www.saucedemo.com");
         LoginPage loginP = new LoginPage(driver);
         loginP.enterUsername("standard_user");
